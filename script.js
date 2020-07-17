bf7bbf189b1750d77e4bfdc77084402537f60a58
EliteAngelus <Angeluselite@gmail.com>
2020-07-17 17:27:59 -0400

    Created a form that takes user input w/ output field

---
+++ script.js
@@ -0,0 +1,18 @@
+inputGrab = () => {
+    // let Value = document.getElementById('input').value;
+
+    let fname = document.getElementById('fname').innerHTML = document.getElementById('first').value;
+    let lname = document.getElementById('lname').innerHTML = document.getElementById('last').value;
+    let email = document.getElementById('email').innerHTML = document.getElementById('mail').value;
+    let location = document.getElementById('location').innerHTML = document.getElementById('place').value;
+
+    console.log(fname);
+    console.log(lname);
+    console.log(email);
+    console.log(location);
+
+}
+
+
+
+// console.log(outputArray);
